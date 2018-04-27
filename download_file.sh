d059de531ba6f550635cacb3a686122b3052ead9
chookhong <33957884+chookhong@users.noreply.github.com>
2018-04-27 11:47:09 +0800

    Delete download_file.sh

--- download_file.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sshpass -e vagrant ssh -c 'cd ~; wget http://releases.ubuntu.com/16.04/ubuntu-16.04.4-server-i386.iso; tar -czf ubuntu-16.04.4-server-i386.iso.tar.gz ubuntu-16.04.4-server-i386.iso; export SSHPASS="testing"; sshpass -e scp ubuntu-16.04.4-server-i386.iso.tar.gz testing@api.server:/home/testing' deploy.server -- -l testing
-
-
